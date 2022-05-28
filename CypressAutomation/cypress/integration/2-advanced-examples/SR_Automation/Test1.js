45987d147fd81fed565d921de4d75126d543f29f
Ratneshwar Jha <ratneshwar.jha@hcl.com>
2022-05-28 11:48:48 +0530

    new file added SR Automation

---
+++ CypressAutomation/cypress/integration/2-advanced-examples/SR_Automation/Test1.js
@@ -0,0 +1,18 @@
+
+/// <reference types="Cypress"/>
+describe('My First Test Suit',function () 
+
+{
+    
+    it('My first Test case', function()
+    {
+cy.visit("https://qa1.myhcl.com/smartrecruit_CBT");
+
+    })
+
+    
+}
+
+
+
+)
