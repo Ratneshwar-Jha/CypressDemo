becb887e66f4c6c84113f31881b6d0c87cca2bcd
Ratneshwar Jha <ratneshwar.jha@hcl.com>
2022-05-28 12:39:02 +0530

    new file added SR GoogleTesting

---
+++ CypressAutomation/cypress/integration/2-advanced-examples/GoogleTesting/Test1.js
@@ -0,0 +1,18 @@
+
+/// <reference types="Cypress"/>
+describe('My First Test Suit',function () 
+
+{
+    
+    it('My first Test case', function()
+    {
+cy.visit("https://www.google.com/");
+
+    })
+
+    
+}
+
+
+
+)
