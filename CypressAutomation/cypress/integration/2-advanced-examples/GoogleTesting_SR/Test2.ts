9a127cc8ac64e3c5ca38ab2f5bb64c4cd418deef
Ratneshwar Jha <ratneshwar.jha@hcl.com>
2022-05-30 18:09:06 +0530

    added typescript test case

---
+++ CypressAutomation/cypress/integration/2-advanced-examples/GoogleTesting_SR/Test2.ts
@@ -0,0 +1,18 @@
+
+/// <reference types="Cypress"/>
+describe('smartrecruit',function () 
+
+{
+    
+    it('My first Test case', function()
+    {
+cy.visit("https://qa1.myhcl.com/smartrecruit_CBT");
+
+    })
+
+    
+}
+
+
+
+)
